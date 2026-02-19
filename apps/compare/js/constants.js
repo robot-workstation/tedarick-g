88e6f74b62288522790da65edbace5999dab13f9
robot-workstation <tvkapora@gmail.com>
2026-02-19 10:36:56 +0300

    Update constants.js

--- apps/compare/js/constants.js
+++ apps/compare/js/constants.js
@@ -1,1 +1,24 @@
 
+import { TR } from './utils.js';
+
+export const ALIAS = new Map([
+  ['ALLEN & HEATH', 'ALLEN HEATH'],
+  ['MARANTZ PROFESSIONAL', 'MARANTZ'],
+  ['RUPERT NEVE DESIGNS', 'RUPERT NEVE'],
+  ['RØDE', 'RODE'],
+  ['RØDE X', 'RODE']
+]);
+
+export const bRaw = s => (s ?? '').toString().trim().toLocaleUpperCase(TR).replace(/\s+/g, ' ');
+export const B   = s => ALIAS.get(bRaw(s)) || bRaw(s);
+export const Bx  = s => bRaw(s);
+
+export const SEO = 'https://www.sescibaba.com/';
+
+export const COLS = [
+  "Sıra No", "Marka",
+  "Ürün Adı (Compel)", "Ürün Adı (Sescibaba)",
+  "Ürün Kodu (Compel)", "Ürün Kodu (Sescibaba)",
+  "Stok (Compel)", "Stok (Sescibaba)", "Stok (Depo)", "Stok Durumu",
+  "EAN (Compel)", "EAN (Sescibaba)", "EAN Durumu"
+];
